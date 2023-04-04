33e240201e58381f6f52d6775ddcfb22c7064d2b
Petr Bouchal <pbouchal@gmail.com>
2023-04-04 17:00:42 +0200

    simplify SV export

--- scripts/isco-2022-pro-sv.R
+++ scripts/isco-2022-pro-sv.R
@@ -1,40 +1,28 @@
-library(dplyr)
-library(ispv)
-library(ggplot2)
-library(ptrr)
-library(writexl)
-
-crlist <- ispv::pv_list_cr(2022)
-
-isco222file <- download.file(crlist$url[c(1, 3)],
-                                   c("data-input/ispv/CR_222_MZS.xlsx",
-                                     "data-input/ispv/CR_222_PLS.xlsx"))
-isco222 <- ispv::pv_cr_monthlypay_isco(c("data-input/ispv/CR_222_MZS.xlsx",
-                                         "data-input/ispv/CR_222_PLS.xlsx"), 7)
-
-isco222 |>
-  filter(isco_id == 2422) |>
-  write_xlsx("data-export/ispv-2022-2422-cr-pro-sv.xlsx")
+source("_targets_packages.R")
 
 targets::tar_load(pv_isco)
 
 pv_isco |>
-  filter(isco_id == 2422) |>
-  write_xlsx("data-export/ispv-2021-2422-reg-pro-sv.xlsx")
+  filter(isco_id %in% c(2422, 1219)) |>
+  write_xlsx("data-export/ispv-2022-2422-reg-pro-sv.xlsx")
 
-isco222 |>
-  filter(isco_id == 2422) |>
+pv_isco |>
+  filter(isco_id %in% c(2422, 1219)) |>
   filter(isco_digits == 4 | is.na(isco_digits)) |>
+  mutate(isco_label = ifelse(isco_id == 1219,
+                        paste0(isco_id, ": Ostatní řídící pracovníci správy podniku a pod. činností"),
+                        paste0(isco_id, ": ",isco_name))) |>
   arrange(sfera) |>
-  ggplot(aes(colour = sfera, y = sfera)) +
+  ggplot(aes(colour = sfera, y = geo)) +
   geom_pointrange(aes(x = pay_median, xmin = pay_q1, xmax = pay_q3),
-                  position = position_dodge2(width = .5)) +
+                  position = position_dodge2(width = .5), key_glyph = draw_key_pointrange_h) +
   geom_point(aes(x = pay_d1), position = position_dodge2(width = .5), shape = 20) +
   geom_point(aes(x = pay_d9), position = position_dodge2(width = .5), shape = 20) +
-  facet_wrap(~ isco_full, ncol = 1, scales = "free_x") +
-  scale_x_number_cz(limits = c(18e3, 135e3),
-                    breaks = seq(from = 30e3, to = 135e3, by = 15e3), scale = 1/1000) +
-  scale_color_manual(values = c("grey40", "darkblue"), guide = "none") +
-  scale_y_discrete(labels = c(PLS = "Platová sféra", MZS = "Mzdová sféra"), breaks = c("PLS", "MZS")) +
+  facet_wrap(~ isco_label, ncol = 1, scales = "free_x") +
+  scale_x_number_cz(limits = c(18e3, 210e3),
+                    breaks = seq(from = 30e3, to = 210e3, by = 15e3), scale = 1/1000) +
+  scale_color_manual(values = c("grey40", "darkblue")) +
+  # scale_y_discrete(labels = c(PLS = "Platová sféra", MZS = "Mzdová sféra"), breaks = c("PLS", "MZS")) +
   theme_ptrr(multiplot = T, gridlines = "x", legend.position = "top") +
-  labs(colour = "Sféra")
+  labs(colour = "Sféra", title = "Platy podle ISCO - Praha a ČR, mzdová a platová sféra",
+       subtitle = "malé tečky: 1. a 9. decil, rozpětí: 1. a 3. kvartil; velká tečka: medián")
