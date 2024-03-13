be349a9ba2929bc41638b0e01532237935af4fdc
Petr Bouchal <pbouchal@gmail.com>
2024-03-13 08:45:49 +0100

    SZU loader streamline

--- scripts/szu-load.R
+++ scripts/szu-load.R
@@ -1,6 +1,9 @@
 library(dplyr)
 library(readr)
+library(tidyr)
 library(readxl)
+library(purrr)
+library(stringr)
 library(ggplot2)
 
 options(scipen = 100)
@@ -14,6 +17,10 @@ read_szu_excel <- function(path, sheet, year) {
     mutate(rok = year, list = sheet, file = basename(path))
 }
 
+szu_names <- c("kap_kod", "kap_zkr", "kap_name", "skut_min", "rozp_schv",
+               "rozp_pzm", "skut", "plneni_proc", "plneni_rozdil", "skut_skut",
+               "rok", "list", "file", "promenna")
+
 szu_uo <- szu_excel_sheets |>
   mutate(file_hash = tools::md5sum(file)) |>
   filter(str_detect(sheets, "ÚO"), str_detect(sheets, "Tab1[10]|Str")) |>
@@ -24,24 +31,15 @@ szu_uo <- szu_excel_sheets |>
   group_by(rok, list, file) |>
   mutate(promenna = if_else((max(x1_2, na.rm = TRUE) > 1e5), "objemy", "pocty")) |>
   ungroup() |>
-  rename(kap_kod = x1,
-         kap_zkr = x2,
-         kap_name = x3,
-         skut_min = x1_2,
-         rozp_schv = x2_2,
-         rozp_pzm = x3_2,
-         skut = x4,
-         plneni_proc = x5,
-         plneni_rozdil = x6,
-         skut_skut = x7
-         ) |>
+  rlang::set_names(szu_names) |>
   drop_na(kap_kod) |>
   mutate(skut_min = if_else(rok < 2013 & promenna == "objemy", skut_min * 1000, skut_min),
          rozp_schv = if_else(rok < 2013 & promenna == "objemy", rozp_schv * 1000, rozp_schv),
          rozp_pzm = if_else(rok < 2013 & promenna == "objemy", rozp_pzm * 1000, rozp_pzm),
          skut = if_else(rok < 2013 & promenna == "objemy", skut * 1000, skut),
          kap_name = if_else(is.na(kap_name), kap_zkr, kap_name)) |>
-  rows_update(read_csv("data-input/kapitoly.csv"), by = "kap_kod")
+  rows_update(read_csv("data-input/kapitoly.csv"), by = "kap_kod") |>
+  relocate(rok, .after = kap_name)
 
 szu_uo |>
   count(rok, kap_kod, promenna) |>
@@ -66,6 +64,9 @@ szu_uo_2009 <- szu_uo |>
          plneni_rozdil = NA,
          rozp_schv = NA, rozp_pzm = NA, skut_skut = NA)
 
+source("scripts/inflation.R")
+source("scripts/wages.R")
+
 szu_uo_wide <- szu_uo |>
   bind_rows(szu_uo_2009) |>
   pivot_wider(id_cols = c(kap_kod, kap_name, rok, promenna, kap_zkr),
@@ -77,15 +78,20 @@ szu_uo_wide <- szu_uo |>
 szu_uo_wide |>
   mutate(plat = objemy_skut/pocty_skut/12) |>
   filter(plat != 0, kap_zkr != "NSA") |>
-  ggplot(aes(rok, plat)) +
+  ggplot(aes(rok, plat/1e3)) +
   geom_line() +
-  geom_line(aes(y = phasal_all), color = "grey") +
+  geom_line(aes(y = phasal_all/1e3), color = "grey") +
   facet_wrap(~kap_zkr)
 
-szu_uo_wide |>
+xszu_uo_wide |>
   filter(is.na(kap_name)) |> View()
   count(kap_kod)
 
-  szu_uo |>
-    distinct(kap_kod, kap_zkr, kap_name) |>
-    format_csv() |> cat()
+szu_uo |>
+  distinct(kap_kod, kap_zkr, kap_name) |>
+  format_csv() |> cat()
+
+dt_orig <- arrow::read_parquet("data-input/szu/data_all.parquet")
+
+dt_orig |>
+  count(faze_rozpoctu)
